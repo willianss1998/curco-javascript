19ab617d3280e57fcca3ec331b925806a060ef1f
willian <williannsantos13@gmail.com>
2025-04-26 13:39:25 -0300

    console.log no codigo linha (27)

--- TrabalhandocomDados02.js
+++ TrabalhandocomDados02.js
@@ -4,6 +4,7 @@ console.log(19.9 * 0.6);
 let precoComDesconto = preco * (1 - desconto);
 console.log(precoComDesconto);
 
-let nome = "caderno";
+let nome = "caderno"; // string -> sequencia de simbolos
 let categoria = "papelaria";
 console.log("produto: " + nome + ", categoria: " + categoria);
+
